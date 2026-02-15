d6f9d2543093a288f21de09c92e3d1316ca95e8f
lucasurielperez <70489713+lucasurielperez@users.noreply.github.com>
2026-02-15 12:42:08 -0300

    Canonicalize core PHP files under project root

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+  'db' => [
+    'host' => 'localhost',
+    'name' => '123',
+    'user' => 'luquxusr',
+    'pass' => '9hSZPdh!Q44yJdkF$xUV#',
+    'charset' => 'utf8mb4',
+  ],
+  'admin_token' => 'CAMBIAME_POR_UN_TOKEN_LARGO_RANDOM',
+  'virus_qr_secret' => 'CAMBIAME_POR_OTRO_SECRETO_LARGO_RANDOM'
+];
